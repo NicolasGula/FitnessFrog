49cc864193390febe031e8247a5936a98f9bdeae
NicolasGula <nicolasgula@outlook.com>
2022-05-04 13:49:15 -0300

    Add project files.

---
+++ Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using Treehouse.FitnessFrog.Data;
+using Treehouse.FitnessFrog.Models;
+
+namespace Treehouse.FitnessFrog.Controllers
+{
+    public class EntriesController : Controller
+    {
+        private EntriesRepository _entriesRepository = null;
+
+        public EntriesController()
+        {
+            _entriesRepository = new EntriesRepository();
+        }
+
+        public ActionResult Index()
+        {
+            List<Entry> entries = _entriesRepository.GetEntries();
+
+            // Calculate the total activity.
+            double totalActivity = entries
+                .Where(e => e.Exclude == false)
+                .Sum(e => e.Duration);
+
+            // Determine the number of days that have entries.
+            int numberOfActiveDays = entries
+                .Select(e => e.Date)
+                .Distinct()
+                .Count();
+
+            ViewBag.TotalActivity = totalActivity;
+            ViewBag.AverageDailyActivity = (totalActivity / (double)numberOfActiveDays);
+
+            return View(entries);
+        }
+
+        public ActionResult Add()
+        {
+            var entry = new Entry()
+            {
+                Date = DateTime.Today
+            };
+
+            ViewBag.ActivitySelectListItems = new SelectList(Data.Data.Activities, "Id", "Name");
+
+            return View(entry);
+        }
+
+        //Asocio un atributo con el metodo AddPost()
+        //Si a Add se le pasa un valor , va a venir a parar a AddPost
+        [HttpPost]
+        public ActionResult Add(Entry entry)
+        {
+
+            if(ModelState.IsValid)
+            {
+                _entriesRepository.AddEntry(entry);
+                //Previene el problema del doble POST
+                //Resuelto con Post/Redirect/Get
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ActivitySelectListItems = new SelectList(Data.Data.Activities, "Id", "Name");
+
+            return View(entry);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            return View();
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            return View();
+        }
+    }
+}
